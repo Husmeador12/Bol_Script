50589c6fcb99221332e4a202b8b54e31283269fd
Husmeador12 <asus12@gmx.com>
2015-07-28 13:01:39 +0200

    Update to 4.6 (5.14)
    Improving

--- iARAM.lua
+++ iARAM.lua
@@ -86,12 +86,12 @@ local wardSlot = nil
 
 
 -----[[ Ignite and Zhonya Globals ]]------
-local ignite = nil
+
 
 
 -----[[ Auto Update Globals ]]------
-local version = 4.5
-local UPDATE_CHANGE_LOG = "Added AutoPlay in mode Alone Mode"
+local version = 4.6
+local UPDATE_CHANGE_LOG = "Improving."
 local UPDATE_HOST = "raw.githubusercontent.com"
 local UPDATE_PATH = "/Husmeador12/Bol_Script/master/iARAM.lua".."?rand="..math.random(1,10000)
 local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
@@ -291,8 +291,9 @@ end
 		--|>Auto Ignite
 		FunctionAutoIgnite()
 		
-		
-				
+		GetPlayer()
+		LoadMapVariables()
+
 end
 
 
@@ -316,6 +317,7 @@ function OnTick()
 	AutoPotions()
 	
 	FollowMinionAlly()
+	HealthAlly()
 	
 end
 
@@ -339,6 +341,7 @@ end
 function Follow()
 	if iARAM.follow and not myHero.dead then
 		stance = 0
+		
 		if Allies() >= 2 then
 			stance = 1		
 		else
@@ -348,45 +351,6 @@ function Follow()
 			Target = findLowHp()
 		else
 			Target = findClosestEnemy()
-		end
-		
-		--|> Alone Mode
-		if stance == 0 then
-			if howlingAbyssMap == true then
-			--[[
-				randomposx = math.random(4700,5100)
-				randomposz = math.random(5600,5900)
-				distance3 = math.random(250,300)
-				distance4 = math.random(250,300)
-				neg3 = 1 
-				neg4 = 1 
-				if myHero.team == TEAM_BLUE then
-					myHero:MoveTo(randomposx*neg3,randomposz-distance4*neg4)
-				else
-					myHero:MoveTo(randomposx*neg3,-randomposz+distance4*neg4)
-				end
-				
-				--myHero:MoveTo(49000,-17000)
-			]]
-			elseif summonersRiftMap == true then
-			--[[
-				timerito = 1			
-				DelayAction(function() 
-					timerito = 2
-					randomposx = math.random(4700,5150)
-					randomposz = math.random(5600,5900)
-					distance3 = math.random(250,300)
-					distance4 = math.random(250,300)
-					neg3 = 1 
-					neg4 = 1 
-					if myHero.team == TEAM_BLUE then
-						myHero:MoveTo(randomposx*neg3,randomposz-distance4*neg4)
-					else
-						myHero:MoveTo(randomposx*neg3,-randomposz+distance4*neg4)
-					end
-				end, timerito)
-				]]
-			end
 		end
 		Allie = followHero()
 		--|>Attacks Champs
@@ -442,9 +406,9 @@ function Follow()
 		end	
 	else
 	--|> Dead
-	--	AutoBuy()
+		AutoBuy()
 	end
-	--AutoBuy()
+	AutoBuy()
 end
 
 function findClosestEnemy()
@@ -535,80 +499,22 @@ end
 
 
 --[[ AutoBuyItems ]]--
-local IDBytes = {
-	[0x00] = 0x10, [0x01] = 0xC5, [0x02] = 0x0C, [0x03] = 0x9F, [0x04] = 0x9C, [0x05] = 0x75, [0x06] = 0xE5, [0x07] = 0x09, [0x08] = 0xC2, [0x09] = 0x43, [0x0A] = 0x97, [0x0B] = 0xB2, 
-	[0x0C] = 0xA9, [0x0D] = 0x9A, [0x0E] = 0x0F, [0x0F] = 0x57, [0x10] = 0x54, [0x11] = 0x51, [0x12] = 0x01, [0x13] = 0x8A, [0x14] = 0x1B, [0x15] = 0xC8, [0x16] = 0xD3, [0x17] = 0x58, 
-	[0x18] = 0x50, [0x19] = 0x23, [0x1A] = 0x4F, [0x1B] = 0xFF, [0x1C] = 0x95, [0x1D] = 0x84, [0x1E] = 0xA1, [0x1F] = 0x8C, [0x20] = 0x69, [0x21] = 0x30, [0x22] = 0xA8, [0x23] = 0xEF, 
-	[0x24] = 0xBF, [0x25] = 0xFC, [0x26] = 0x59, [0x27] = 0x4E, [0x28] = 0x39, [0x29] = 0x5D, [0x2A] = 0x1F, [0x2B] = 0xE8, [0x2C] = 0xB7, [0x2D] = 0xAE, [0x2E] = 0xAC, [0x2F] = 0x9E, 
-	[0x30] = 0xF8, [0x31] = 0xE0, [0x32] = 0x62, [0x33] = 0xA0, [0x34] = 0xC7, [0x35] = 0xD4, [0x36] = 0xBD, [0x37] = 0x0D, [0x38] = 0x5F, [0x39] = 0xE3, [0x3A] = 0x2E, [0x3B] = 0xD5, 
-	[0x3C] = 0xCE, [0x3D] = 0x6D, [0x3E] = 0x81, [0x3F] = 0x63, [0x40] = 0xC0, [0x41] = 0xCF, [0x42] = 0x40, [0x43] = 0x1C, [0x44] = 0xB8, [0x45] = 0x3B, [0x46] = 0xD9, [0x47] = 0x94, 
-	[0x48] = 0xBA, [0x49] = 0x88, [0x4A] = 0xC6, [0x4B] = 0x27, [0x4C] = 0x48, [0x4D] = 0x3E, [0x4E] = 0x1E, [0x4F] = 0xD0, [0x50] = 0x15, [0x51] = 0x7E, [0x52] = 0x08, [0x53] = 0x7C, 
-	[0x54] = 0x70, [0x55] = 0x04, [0x56] = 0x41, [0x57] = 0xB1, [0x58] = 0xA6, [0x59] = 0xD1, [0x5A] = 0x4D, [0x5B] = 0xC3, [0x5C] = 0x05, [0x5D] = 0x90, [0x5E] = 0xC4, [0x5F] = 0x98, 
-	[0x60] = 0xFE, [0x61] = 0x35, [0x62] = 0xED, [0x63] = 0xA5, [0x64] = 0xC9, [0x65] = 0x85, [0x66] = 0xF9, [0x67] = 0x74, [0x68] = 0x96, [0x69] = 0x8D, [0x6A] = 0xBC, [0x6B] = 0xFA, 
-	[0x6C] = 0x31, [0x6D] = 0x11, [0x6E] = 0x5B, [0x6F] = 0xAD, [0x70] = 0x4C, [0x71] = 0xA2, [0x72] = 0xB3, [0x73] = 0xEE, [0x74] = 0xE6, [0x75] = 0xD8, [0x76] = 0x02, [0x77] = 0x3C, 
-	[0x78] = 0x8B, [0x79] = 0xE2, [0x7A] = 0x7A, [0x7B] = 0xDD, [0x7C] = 0x4A, [0x7D] = 0x00, [0x7E] = 0x6A, [0x7F] = 0xE9, [0x80] = 0x7B, [0x81] = 0xF6, [0x82] = 0x89, [0x83] = 0x2C, 
-	[0x84] = 0xEB, [0x85] = 0xCA, [0x86] = 0x6C, [0x87] = 0x2A, [0x88] = 0xDB, [0x89] = 0xE7, [0x8A] = 0xAF, [0x8B] = 0x4B, [0x8C] = 0x0E, [0x8D] = 0x16, [0x8E] = 0x2F, [0x8F] = 0x76, 
-	[0x90] = 0x91, [0x91] = 0xF1, [0x92] = 0x92, [0x93] = 0x66, [0x94] = 0x44, [0x95] = 0xAA, [0x96] = 0x72, [0x97] = 0xF7, [0x98] = 0xF4, [0x99] = 0x93, [0x9A] = 0x2D, [0x9B] = 0x80, 
-	[0x9C] = 0x03, [0x9D] = 0x65, [0x9E] = 0x42, [0x9F] = 0xF2, [0xA0] = 0x18, [0xA1] = 0xCC, [0xA2] = 0x8E, [0xA3] = 0x99, [0xA4] = 0x14, [0xA5] = 0x7D, [0xA6] = 0xC1, [0xA7] = 0xD2, 
-	[0xA8] = 0x5E, [0xA9] = 0x33, [0xAA] = 0x64, [0xAB] = 0x3F, [0xAC] = 0x38, [0xAD] = 0x0A, [0xAE] = 0xD6, [0xAF] = 0xD7, [0xB0] = 0x6E, [0xB1] = 0xF3, [0xB2] = 0x1A, [0xB3] = 0xE4, 
-	[0xB4] = 0x68, [0xB5] = 0x13, [0xB6] = 0x46, [0xB7] = 0xB0, [0xB8] = 0x73, [0xB9] = 0x12, [0xBA] = 0x06, [0xBB] = 0x5C, [0xBC] = 0x37, [0xBD] = 0x86, [0xBE] = 0x61, [0xBF] = 0x78, 
-	[0xC0] = 0xB9, [0xC1] = 0xF0, [0xC2] = 0x21, [0xC3] = 0xBE, [0xC4] = 0x24, [0xC5] = 0xEA, [0xC6] = 0x32, [0xC7] = 0xDE, [0xC8] = 0xDA, [0xC9] = 0x8F, [0xCA] = 0x47, [0xCB] = 0x1D, 
-	[0xCC] = 0xDF, [0xCD] = 0xBB, [0xCE] = 0x3D, [0xCF] = 0xE1, [0xD0] = 0x6F, [0xD1] = 0xA3, [0xD2] = 0x55, [0xD3] = 0xAB, [0xD4] = 0x83, [0xD5] = 0x17, [0xD6] = 0x7F, [0xD7] = 0x2B, 
-	[0xD8] = 0x34, [0xD9] = 0x52, [0xDA] = 0x87, [0xDB] = 0xB6, [0xDC] = 0x45, [0xDD] = 0xFD, [0xDE] = 0x28, [0xDF] = 0xB4, [0xE0] = 0x26, [0xE1] = 0xCB, [0xE2] = 0xA4, [0xE3] = 0xA7, 
-	[0xE4] = 0xF5, [0xE5] = 0xCD, [0xE6] = 0x77, [0xE7] = 0xDC, [0xE8] = 0x22, [0xE9] = 0x6B, [0xEA] = 0xEC, [0xEB] = 0x49, [0xEC] = 0x29, [0xED] = 0x9D, [0xEE] = 0x79, [0xEF] = 0x67, 
-	[0xF0] = 0x3A, [0xF1] = 0x82, [0xF2] = 0x5A, [0xF3] = 0x9B, [0xF4] = 0xB5, [0xF5] = 0x0B, [0xF6] = 0x56, [0xF7] = 0x71, [0xF8] = 0x19, [0xF9] = 0x25, [0xFA] = 0x07, [0xFB] = 0xFB, 
-	[0xFC] = 0x20, [0xFD] = 0x60, [0xFE] = 0x36, [0xFF] = 0x53,
-}
-
-function OnRecvPacket(p)
-	if VIP_USER and UP_TO_DATE then
-		if p.header == 0x005C then
-			p.pos=2
-			if p:DecodeF() == myHero.networkID then
-				p.pos=12
-				local bytes = {}
-				for i=4, 1, -1 do
-					bytes[i] = IDBytes[p:Decode1()]
-				end
-				lastBoughtItem = bit32.bxor(bit32.lshift(bit32.band(bytes[1],0xFF),24),bit32.lshift(bit32.band(bytes[2],0xFF),16),bit32.lshift(bit32.band(bytes[3],0xFF),8),bit32.band(bytes[4],0xFF))
-			end
-		end
-	end
-end
-
-function BuyItem1(id)
-
-	local rB = {}
-	for i=0, 255 do rB[IDBytes[i]] = i end
-	local p = CLoLPacket(0x0008)
-	p.vTable = 0xEAC648
-	p:EncodeF(myHero.networkID)
-	local b1 = bit32.lshift(bit32.band(rB[bit32.band(bit32.rshift(bit32.band(id,0xFFFF),24),0xFF)],0xFF),24)
-	local b2 = bit32.lshift(bit32.band(rB[bit32.band(bit32.rshift(bit32.band(id,0xFFFFFF),16),0xFF)],0xFF),16)
-	local b3 = bit32.lshift(bit32.band(rB[bit32.band(bit32.rshift(bit32.band(id,0xFFFFFFFF),8),0xFF)],0xFF),8)
-	local b4 = bit32.band(rB[bit32.band(id ,0xFF)],0xFF)
-	p:Encode4(bit32.bxor(b1,b2,b3,b4))
-	p:Encode4(0xE1240DFD)
-	SendPacket(p)
-end
 
 function AutoBuy()
+	buyIndex = 1
 	if (not VIP_USER) then
 		_AutoupdaterMsg('[AutoBuy] Non-VIP Not Supported!');
 	end;
-	if VIP_USER and iARAM.autobuy and InFountain() or myHero.dead then
-			-- Item purchases
-		if GetTickCount() > lastBuy + buyDelay then
-			if lastBoughtItem == shopList[nextbuyIndex] then
-				--Last Buy successful
-				nextbuyIndex = nextbuyIndex + 1
-			else
-				lastBuy = GetTickCount()
-				if VIP_USER and UP_TO_DATE then BuyItem1(shopList[nextbuyIndex])
-				else BuyItem(shopList[nextbuyIndex]) end
-			end
+	if VIP_USER and iARAM.autobuy and InFountain() or myHero.dead or shopList[buyIndex] ~= 0 then
+			local itemval = shopList[buyIndex]	
+			BuyItem(itemval)
+				if GetInventorySlotItem(shopList[buyIndex]) ~= nil then
+					--Last Buy successful
+					buyIndex = buyIndex + 1
+					buyItems()
+				end
 		end
-	end
+
 end
 
 
@@ -1002,10 +908,6 @@ function IgniteCheck()
 			iARAM:addParam("autoIgnite", "Auto Ignite", SCRIPT_PARAM_ONOFF, true)
 	end
 end
- 
-function FunctionAutoZhonya()
-
-end
 
 
 ---------[[ Auto Good luck and have fun ]]---------
@@ -1016,9 +918,19 @@ Phrases2 = {"c´mon guys", "we can do it", "This is my winner team", "It doesnt 
 	CountTimer = 15
 	if os.clock() < CountTimer then return end
 		SendChat(Text1[math.random(#Text1)])
+
 		CountTimer = os.clock() + math.random(0.5,2)
 		
-	
+	if GetInGameTimer() < 15 then
+				DelayAction(function()
+				if myHero.team == TEAM_BLUE then
+					myHero:MoveTo(myHero.x*5,myHero.z*5)
+				else
+					myHero:MoveTo(myHero.x*5,myHero.z*5)
+				end
+				end, 15-GetInGameTimer())
+			end
+			
 	
 	if GetInGameTimer() < 333 then
 		DelayAction(function()
@@ -1191,10 +1103,10 @@ end
 
 function LoadMapVariables()
 	gameState = GetGame()
+	--print(gameState.map.shortName)
 	if gameState.map.shortName then
 		if gameState.map.shortName == "summonerRift" then
 			summonersRiftMap = true
-			--print("summonerRift")
 		else
 			summonersRiftMap = false
 		end
@@ -1451,7 +1363,10 @@ function AutoLevel:__init(table)
 				end
 			end
 		end)
+	else
+	_AutoupdaterMsg("You are not VIP user: AutoLevel disabled")
 	end
+	
 end
 
 function AutoLevel:LevelSpell(id)
@@ -1563,25 +1478,66 @@ end
 
 function FollowMinionAlly()
 	if stance == 0 and iARAM.follow then
-				allyMinions = minionManager(MINION_ALLY, 90000, player, MINION_SORT_HEALTH_DEC)
-				allyMinions:update()
-				local player = GetMyHero()
-				local tick = 0
-				local delay = 400
-				local myTarget = ts.target
-		if heroCanMove() then
-			for index, allyminion in pairs(allyMinions.objects) do
-				if GetDistance(allyminion, myHero) <= 1600 and GetTickCount() > tick + delay then
-					distance1 = math.random(250,300)
-					distance2 = math.random(230,290)
-					neg1 = -1 
-					neg2 = -1 	
-					if myHero.team == TEAM_BLUE then
-						myHero:MoveTo(allyminion.x+distance1*neg1,allyminion.z+distance2*neg2)
-						tick = GetTickCount()
-					else
-						myHero:MoveTo(allyminion.x-distance1*neg1,allyminion.z-distance2*neg2)
-						tick = GetTickCount()
+		if summonersRiftMap == true then
+			CountTimer = 15
+			if os.clock() < CountTimer then return end
+			CountTimer = os.clock() + math.random(0.5,2)
+			if GetInGameTimer() < 15 then
+				DelayAction(function()
+				--print("lol")
+				if myHero.team == TEAM_BLUE then
+					myHero:MoveTo(myHero.x*5,myHero.z*5)
+				else
+					myHero:MoveTo(myHero.x*5,myHero.z*5)
+				end
+				end, 15-GetInGameTimer())
+			end
+			
+			allyMinions = minionManager(MINION_ALLY, 3000, player, MINION_SORT_HEALTH_DEC)
+			allyMinions:update()
+			local player = GetMyHero()
+			local tick = 0
+			local delay = 400
+			local myTarget = ts.target
+			if heroCanMove() then
+				for index, allyminion in pairs(allyMinions.objects) do
+					if GetDistance(allyminion, myHero) <= 1600 and GetTickCount() > tick + delay then
+						distance1 = math.random(130,250)
+						distance2 = math.random(130,250)
+						neg1 = -1 
+						neg2 = -1 	
+						if myHero.team == TEAM_BLUE then
+							myHero:MoveTo(allyminion.x+distance1*neg1,allyminion.z+distance2*neg2)
+							tick = GetTickCount()
+						else
+							myHero:MoveTo(allyminion.x-distance1*neg1,allyminion.z-distance2*neg2)
+							tick = GetTickCount()
+						end
+					end
+				end
+			end
+		end
+		if howlingAbyssMap == true then
+			allyMinions = minionManager(MINION_ALLY, 3000, player, MINION_SORT_HEALTH_DEC)
+			allyMinions:update()
+			local player = GetMyHero()
+			local tick = 0
+			local delay = 400
+			local myTarget = ts.target
+			if heroCanMove() then
+				for index, allyminion in pairs(allyMinions.objects) do
+					if GetDistance(allyminion, myHero) <= 3000 and GetTickCount() > tick + delay then
+						distance1 = math.random(130,250)
+						distance2 = math.random(130,250)
+						neg1 = -1 
+						neg2 = -1 	
+						if myHero.team == TEAM_BLUE then
+							myHero:MoveTo(allyminion.x+distance1*neg1,allyminion.z+distance2*neg2)
+							tick = GetTickCount()
+						else
+							myHero:MoveTo(allyminion.x-distance1*neg1,allyminion.z-distance2*neg2)
+							tick = GetTickCount()
+						end
 					end
 				end
 			end
@@ -1589,3 +1545,45 @@ function FollowMinionAlly()
 	end
 end
 
+function HealthAlly()
+	if stance == 1 or stance == 3 and iARAM.follow then
+	local champ = player.charName
+	local ally = GetPlayer(myHero.team, false, false, myHero, 450, "health")
+		if ally ~= nil and (ally.health/ally.maxHealth) < 99999 then
+			if champ == "Soraka" then
+				CastSpell(_W, ally)
+			elseif champ == "Taric" then  
+				CastSpell(_Q, ally)
+			elseif champ == "Nami" then  
+				CastSpell(_W, ally)
+			elseif champ == "Sona" then  
+				CastSpell(_W, ally)
+			end
+		end
+	end
+end
+
+function GetPlayer(team, includeDead, includeSelf, distanceTo, distanceAmount, resource)
+	local target = nil
+	for i=1, heroManager.iCount do
+		local member = heroManager:GetHero(i)
+		if member ~= nil and member.type == "AIHeroClient" and member.team == team and (member.dead ~= true or includeDead) then
+			if member.charName ~= myHero.charName or includeSelf then
+				if distanceAmount == GLOBAL_RANGE or member:GetDistance(distanceTo) <= distanceAmount then
+					if target == nil then target = member end
+
+					if resource == "health" then --least health
+						if member.health < target.health then target = member end
+					elseif resource == "mana" then --least mana
+						if member.mana < target.mana then target = member end
+					elseif resource == "AD" then --highest AD
+						if member.totalDamage > target.totalDamage then target = member end
+					elseif resource == NO_RESOURCE then
+						return member -- as any member is eligible
+					end
+				end
+			end
+		end
+	end
+	return target
+end
